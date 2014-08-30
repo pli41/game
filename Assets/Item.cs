a8c1495922fe3a2710ba08928a79821e4527fee2
Peilin Li <lipeilin@gatech.edu>
2014-08-30 16:02:41 -0400

    readd

---
+++ Assets/Item.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Item : MonoBehaviour {
+
+
+	public float heal = 40;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnCollisionEnter(Collision collision){
+		if(collision.gameObject.tag == "Player"){
+			collision.gameObject.SendMessage("playerOnHeal", heal);
+			Destroy(gameObject);
+		}
+	}
+}
---
+++ Assets/Scripts/Mob.cs
@@ -0,0 +1,128 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Mob : MonoBehaviour {
+	public enum state{idle, move, attack, die};
+	public state mobState = state.idle;
+	public float speed;
+	public float attackRange;
+	public float alarmRange;
+	public float maxHealth;
+	public float hp;
+	public float damage;
+
+
+	private Color startcolor;
+	private bool mouseOver;
+
+	public Material body;
+	public Material head;
+
+	public Transform playerTransform;
+	public CharacterController controller;
+
+	public AnimationClip run;
+	public AnimationClip idle;
+	public AnimationClip attack;
+	public AnimationClip die;
+
+	// Use this for initialization
+	void Start () {
+		body.color = Color.white;
+		body.color = Color.white;
+		startcolor = body.color;
+		mouseOver = false;
+		hp = maxHealth;
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if(hp <= 0){
+			mobState = state.die;
+		}
+		else{
+			if(!mouseOver){
+				OnMouseNotOver();
+			}
+			else{
+				mouseOver = false;
+			}
+
+			if (InAttackRange()){
+				mobState = state.attack;
+			}
+			else if (InAlarmRange ()){
+				mobState = state.move;
+			}
+			else{
+				mobState = state.idle;
+			}
+		}
+
+		if(mobState == state.die){
+			animation.CrossFade (die.name);
+			Invoke("Die", 2.0f);
+		}
+		else if (mobState == state.attack){
+			Attack();
+		}
+		else if (mobState == state.move){
+			Run();
+		}
+		else{
+			Idle();
+		}
+	
+
+
+	}
+
+	void MobOnHit(float damage){
+		hp -= damage;
+		Debug.Log ("Mob health =" + hp);
+	}
+
+
+	public bool InAttackRange(){
+		return Vector3.Distance (transform.position, playerTransform.position) < attackRange;
+	}
+
+	bool InAlarmRange(){
+		return Vector3.Distance (transform.position, playerTransform.position) < alarmRange;
+	}
+
+	void Die(){
+		Debug.Log("Mob dies");
+		Destroy (gameObject);
+	}
+
+	void Attack(){
+		animation.CrossFade(attack.name);
+	}
+
+	void Idle(){
+		animation.CrossFade (idle.name);
+	}
+
+	void Run(){
+		transform.LookAt (playerTransform.position);
+		controller.SimpleMove (transform.forward * speed);
+		animation.CrossFade(run.name);
+	}
+
+	void OnMouseOver(){
+		mouseOver = true;
+		body.color = Color.red;
+		head.color = Color.red;
+	}
+
+	void OnMouseNotOver(){
+		body.color = startcolor;
+		head.color = startcolor;
+	}
+
+}
+
+
